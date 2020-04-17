109d00ceeb9cfad029eba56df26f01f017666379
peyang-Celeron <42040068+peyang-Celeron@users.noreply.github.com>
2020-04-18 07:18:47 +0900

    CRLF to LF Maintainability fixed.
    Signed-off-by: peyang-Celeron <mikasa.chibimasu@gmail.com>

--- src/main/java/ml/peya/plugins/Events/Inventorys/InventoryClickEvent.java
+++ src/main/java/ml/peya/plugins/Events/Inventorys/InventoryClickEvent.java
@@ -3,7 +3,6 @@ package ml.peya.plugins.Events.Inventorys;
 import ml.peya.plugins.*;
 import ml.peya.plugins.Enums.*;
 import ml.peya.plugins.Inventorys.*;
-import org.bukkit.*;
 import org.bukkit.entity.*;
 import org.bukkit.event.*;
 import org.bukkit.inventory.*;
@@ -19,7 +18,7 @@ public class InventoryClickEvent implements Listener
             InventoryView playerInventory = player.getOpenInventory();
             org.bukkit.inventory.Inventory inventory = playerInventory.getTopInventory();
             final ItemStack[] inItems = {inventory.getItem(49)};
-            if (inItems[0] == null || inItems[0].getType() == Material.AIR)
+            if (InventoryItem.isAirOrNull(inItems[0]))
                 return;
             if (inItems[0].getItemMeta().getDisplayName().equals(Atm.language.translateString("word.in")))
             {
